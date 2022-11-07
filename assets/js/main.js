43af3eddd657c1bb19dc81a05423e0bde6b075e6
mecarm <melecarmine99@gmail.com>
2022-11-07 15:19:47 +0100

    creo cartella con file

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*
+Snack 1
+Dato l'array di nomi:
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+e dati due numeri min e max (min più piccolo di max).
+Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+Eseguiamo questo esercizio prima con forEach e poi con filter.
+esempio: dati i valori min: 2 e come max: 4 i nomi nell'array nuovo saranno i nomi compresi tra queste posizioni 2 e 4
+*/

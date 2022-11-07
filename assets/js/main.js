ce53072d1be96ab64ee16fe0b62bc2f5103753a8
mecarm <melecarmine99@gmail.com>
2022-11-07 16:13:56 +0100

    JSnack 2 completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,60 @@ const newArray = myArray.filter((element, index) => { // filtra element(elementi
     else{
         return false;
     }
-
 });
 console.log(newArray);
+
+/*
+Snack 2
+Dato un elenco degli studenti di una facoltà, con il totale dei loro voti
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ];
+1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.
+2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
+3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+*/
+
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ];
+
+const targhe = students.map((element) => {
+    return element.name.toUpperCase();
+})
+
+console.log(targhe);
+
+const studentiPremiati = students.filter((element) => {
+    if(element.grades >= 70){
+        return true;
+    }
+    else{
+        return false
+    }
+});
+
+console.log(studentiPremiati);
+
+const studentiIdSuperiore = students.filter((element) => {
+    if(element.grades >= 70 && element.id >= 120){
+        return true;
+    }
+    else{
+        return false
+    }
+});
+
+console.log(studentiIdSuperiore);
